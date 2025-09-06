8f067dd0e5fe282049c1577df241aed28dfe9a04
rorikstr <slava.070299@gmail.com>
2025-09-06 19:00:22 +0300

    to tpp

---
+++ src/insert.tpp
@@ -0,0 +1,37 @@
+#ifndef INSERT_TPP
+#define INSERT_TPP
+
+#include "rbtree.h"
+
+template <typename T, typename Compare>
+void RBTree<T, Compare>::insert(const T& val_) {
+    Node<T>* new_node = new Node<T>(val_);
+    if (!root_) {
+        root_ = new_node;
+        root_->color_ = Color::Black;
+        return;
+    }
+    Node<T>* curr = root_;
+    Node<T>* parent_ = nullptr;
+    // Поиск места для вставки
+    while (curr) {
+        parent_ = curr;
+        if (comp_(val_, curr->val_)) {
+            curr = curr->left_;
+        } else if (comp_(curr->val_, val_)) {
+            curr = curr->right_;
+        } else {
+            delete new_node;
+            return;
+        }
+    }
+    new_node->parent_ = parent_;
+    if (comp_(val_, parent_->val_)) {
+        parent_->left_ = new_node;
+    } else {
+        parent_->right_ = new_node;
+    }
+    // TODO: Реализовать полную вставку с компаратором
+}
+
+#endif
--- src/rbtree.h
+++ src/rbtree.h
@@ -27,6 +27,10 @@ class RBTree {
 public:
     RBTree() : root_(nullptr), comp_(Compare()) {}
     ~RBTree() { clear(root_); }
+    RBTree(const RBTree& other);
+    RBTree(RBTree&& other) noexcept;
+    RBTree& operator=(const RBTree& other);
+    RBTree& operator=(RBTree&& other) noexcept;
     void insert(const T& val_);
     [[nodiscard]] Node<T>* get_root() const { return root_; }
 
@@ -42,6 +46,33 @@ private:
 };
 
 template <typename T, typename Compare>
+RBTree<T, Compare>::RBTree(const RBTree& other)
+    : root_(nullptr), comp_(other.comp_) {
+    if (other.root_) {
+        root_ = copy_subtree(other.root_, nullptr);
+    }
+}
+
+template <typename T, typename Compare>
+Node<T>* copy_subtree(Node<T>* node, Node<T>* parent) {
+    if (!node) return nullptr;
+    Node<T>* new_node = new Node<T>(node->val_);
+    new_node->color_ = node->color_;
+    new_node->parent_ = parent;
+    new_node->left_ = copy_subtree(node->left_, new_node);
+    new_node->right_ = copy_subtree(node->right_, new_node);
+    return new_node;
+}
+
+template <typename T, typename Compare>
+RBTree<T, Compare>::RBTree(RBTree&& other) noexcept
+    : root_(other.root_), comp_(std::move(other.comp_)) {
+    other.root_ = nullptr;
+}
+
+#include "operators.tpp"
+
+template <typename T, typename Compare>
 void RBTree<T, Compare>::clear(Node<T>* node) {
     if (node) {
         clear(node->left_);
@@ -50,73 +81,8 @@ void RBTree<T, Compare>::clear(Node<T>* node) {
     }
 }
 
-template <typename T, typename Compare>
-void RBTree<T, Compare>::left_rotate(Node<T>* up_to_leftdown) { // 6-7 свапов указателей
-    Node<T>* downright_to_up = up_to_leftdown->right_;
-    up_to_leftdown->right_ = downright_to_up->left_;
-    if (nullptr != downright_to_up->left_) { // если у правого есть левый потомок
-        downright_to_up->left_->parent_ = up_to_leftdown;
-    }
-    downright_to_up->parent_ = up_to_leftdown->parent_;
-    if (nullptr == up_to_leftdown->parent_) { // если верхний - корень
-        root_ = downright_to_up;
-    } else if (up_to_leftdown == up_to_leftdown->parent_->left_) { // если не корень, меняем 
-        up_to_leftdown->parent_->left_ = downright_to_up;
-    } else {
-        up_to_leftdown->parent_->right_ = downright_to_up;
-    }
-    downright_to_up->left_ = up_to_leftdown;
-    up_to_leftdown->parent_ = downright_to_up;
-}
+#include "balancing.tpp"
 
-template <typename T, typename Compare>
-void RBTree<T, Compare>::right_rotate(Node<T>* up_to_rightdown) { // 6-7 свапов указателей
-    Node<T>* downleft_to_up = up_to_rightdown->left_;
-    up_to_rightdown->left_ = downleft_to_up->right_;
-    if (nullptr != downleft_to_up->right_) { // если у правого есть левый потомок
-        downleft_to_up->right_->parent_ = up_to_rightdown;
-    }
-    downleft_to_up->parent_ = up_to_rightdown->parent_;
-    if (nullptr == up_to_rightdown->parent_) { // если верхний - корень
-        root_ = downleft_to_up;
-    } else if (up_to_rightdown == up_to_rightdown->parent_->right_) { // если не корень, меняем 
-        up_to_rightdown->parent_->right_ = downleft_to_up;
-    } else {
-        up_to_rightdown->parent_->left_ = downleft_to_up;
-    }
-    downleft_to_up->right_ = up_to_rightdown;
-    up_to_rightdown->parent_ = downleft_to_up;
-}
-
-template <typename T, typename Compare>
-void RBTree<T, Compare>::insert(const T& val_) {
-    Node<T>* new_node = new Node<T>(val_);
-    if (!root_) {
-        root_ = new_node;
-        root_->color_ = Color::Black;
-        return;
-    }
-    Node<T>* curr = root_;
-    Node<T>* parent_ = nullptr;
-    // Поиск места для вставки
-    while (curr) {
-        parent_ = curr;
-        if (comp_(val_, curr->val_)) {
-            curr = curr->left_;
-        } else if (comp_(curr->val_, val_)) {
-            curr = curr->right_;
-        } else {
-            delete new_node;
-            return;
-        }
-    }
-    new_node->parent_ = parent_;
-    if (comp_(val_, parent_->val_)) {
-        parent_->left_ = new_node;
-    } else {
-        parent_->right_ = new_node;
-    }
-    // TODO: Реализовать полную вставку с компаратором
-}
+#include "insert.tpp"
 
 #endif // RBTREE_H
